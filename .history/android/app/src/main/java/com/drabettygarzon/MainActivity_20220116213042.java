f526751c527be62093b4f5c97c081e2d98b93690
Lenin Valenzuela <graybismuthmaster@hotmail.com>
2022-01-17 11:37:14 -0500

    Registro y Login completo

---
+++ .history/android/app/src/main/java/com/drabettygarzon/MainActivity_20220116213042.java
@@ -0,0 +1,29 @@
+package com.drabettygarzon;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "draBettyGarzon";
+  }
+
+  @Override
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+      new MainReactPackage(), // <---- add comma
+      new RNFSPackage() // <---------- add package
+    );
+  }
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+}
